4843258dcef14224dcc2609d7eb31ebc9ea80319
nordluma <maxnordlund.95@hotmail.com>
2025-06-16 00:04:22 +0300

    feat: add flag for verbose output

--- main.py
+++ main.py
@@ -2,18 +2,32 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-prompt = " ".join(sys.argv[1:])
-if not prompt:
+user_prompt = sys.argv[1]
+if not user_prompt:
     print("A prompt is required")
     exit(1)
 
-response = client.models.generate_content(model="gemini-2.0-flash-001", contents=prompt)
+verbose_output = False
+if len(sys.argv) >= 3 and sys.argv[2] == "--verbose":
+    verbose_output = True
+
+if verbose_output:
+    print(f"User prompt: {user_prompt}")
+
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+
+
+response = client.models.generate_content(
+    model="gemini-2.0-flash-001", contents=messages
+)
 
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+if verbose_output:
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
