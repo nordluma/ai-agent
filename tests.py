b34d8b95c23d8bb9cdd8f2a415e7e41da88ed06e
nordluma <maxnordlund.95@hotmail.com>
2025-06-19 23:55:34 +0300

    tests: `get_file_content` works as expected

--- tests.py
+++ tests.py
@@ -1,17 +1,14 @@
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 
 def test():
-    result = get_files_info("calculator", ".")
-    print(result)
-    print("")
-
-    result = get_files_info("calculator", "pkg")
-    print("Result for 'pkg' directory:")
+    result = get_file_content("calculator", "main.py")
     print(result)
 
-    result = get_files_info("calculator", "../")
-    print("Result for '../' directory:")
+    result = get_file_content("calculator", "pkg/calculator.py")
+    print(result)
+
+    result = get_file_content("calculator", "bin/cat")
     print(result)
 
 
