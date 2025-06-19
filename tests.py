8e779a3c13ad021fbb1ee7272feaf48a06cd5d45
nordluma <maxnordlund.95@hotmail.com>
2025-06-19 23:33:16 +0300

    test: `get_files_info` works as expected

---
+++ tests.py
@@ -0,0 +1,19 @@
+from functions.get_files_info import get_files_info
+
+
+def test():
+    result = get_files_info("calculator", ".")
+    print(result)
+    print("")
+
+    result = get_files_info("calculator", "pkg")
+    print("Result for 'pkg' directory:")
+    print(result)
+
+    result = get_files_info("calculator", "../")
+    print("Result for '../' directory:")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
