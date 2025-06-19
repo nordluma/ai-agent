eb59567e0f48fcba2e9525c20f0149d548c21a85
nordluma <maxnordlund.95@hotmail.com>
2025-06-20 00:32:34 +0300

    style(functions): print filename without path

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -5,20 +5,19 @@ MAX_CHARS = 10_000
 
 def get_file_content(working_directory, file_path):
     abs_working_dir = os.path.abspath(working_directory)
-    file_path = os.path.abspath(os.path.join(working_directory, file_path))
-    if not file_path.startswith(abs_working_dir):
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+    if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
-    if not os.path.isfile(file_path):
+    if not os.path.isfile(abs_file_path):
         return f'Error: File not found or is not a regular file: "{file_path}"'
 
     try:
-        with open(file_path, "r") as f:
+        with open(abs_file_path, "r") as f:
             file_content = f.read(MAX_CHARS)
 
             if len(file_content) >= MAX_CHARS:
-                return (
-                    file_content
-                    + f"... File {file_path} truncated at {MAX_CHARS} characters"
+                file_content += (
+                    f"[... File {file_path} truncated at {MAX_CHARS} characters]"
                 )
             return file_content
     except Exception as e:
