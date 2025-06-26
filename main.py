e98760c667f289704790f385267091620dbd4e05
nordluma <maxnordlund.95@hotmail.com>
2025-06-26 23:51:25 +0300

    feat: define a system prompt for llm

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
+SYSTEM_PROMPT = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
@@ -24,7 +26,9 @@ messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=messages
+    model="gemini-2.0-flash-001",
+    contents=messages,
+    config=types.GenerateContentConfig(system_instruction=SYSTEM_PROMPT),
 )
 
 print(response.text)
