01f1ee08915f736999dbc059518b3257348e2ed5
nordluma <maxnordlund.95@hotmail.com>
2025-08-13 22:12:12 +0300

    feat(main): allow agent to call functions

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_file
-from functions.write_file import schema_write_file
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.write_file import schema_write_file, write_file
 
 SYSTEM_PROMPT = """
 You are a helpful AI coding agent.
@@ -68,8 +68,56 @@ def generate_content(client, messages, verbose):
     if not response.function_calls:
         return response.text
 
+    fn_responses = []
     for fn_call_part in response.function_calls:
+        fn_call_result = call_function(fn_call_part, verbose)
+        if not fn_call_result.parts or not fn_call_result.parts[0].function_response:
+            raise Exception("empty function call result")
+
+        if verbose:
+            print(f"-> {fn_call_result.parts[0].function_response.response}")
+        fn_responses.append(fn_call_result.parts[0])
+
+    if not fn_responses:
+        raise Exception("no function responses generated")
+
+
+def call_function(fn_call_part: types.FunctionCall, verbose=False):
+    if verbose:
         print(f"Calling function: {fn_call_part.name}({fn_call_part.args})")
+    else:
+        print(f" - Calling function: {fn_call_part.name}")
+
+    fn_map = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file": write_file,
+    }
+
+    fn_name = fn_call_part.name
+    if fn_name not in fn_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=fn_name or "unknown",
+                    response={"error": f"Unknown function: {fn_name}"},
+                )
+            ],
+        )
+
+    args = dict(fn_call_part.args or {})
+    args["working_directory"] = "./calculator"
+    fn_result = fn_map[fn_name](**args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=fn_name, response={"result": fn_result}
+            )
+        ],
+    )
 
 
 def usage():
